17fdd5173132582cda51a92ac3bd47d35e1da6cc
Josh Adell <josh.adell@gmail.com>
2013-11-20 18:37:30 -0500

    Delete and cancel box creation

--- src/js/controllers/box.js
+++ src/js/controllers/box.js
@@ -1,2 +1,19 @@
 App.BoxController = Ember.ObjectController.extend({
+
+	actions: {
+		saveBox: function () {
+			var box = this.get('content');
+			box.save();
+		},
+
+		deleteBox: function () {
+			if (!confirm('Are you sure? This operation cannot be undone.')) {
+				return;
+			}
+
+			this.get('store').deleteRecord(this.get('content'));
+			this.transitionToRoute('boxes');
+		}
+	}
+
 });
--- src/js/models/plant.js
+++ src/js/models/plant.js
@@ -8,20 +8,20 @@ App.Plant.FIXTURES = [{
 	name: "Peas",
 	icon: {
 		small: 'http://placehold.it/32x32/00ff00',
-		large: 'http://placehold.it/128x128/00ff00'
+		large: 'http://placehold.it/64x64/00ff00'
 	}
 },{
 	id: 2,
 	name: "Carrots",
 	icon: {
 		small: 'http://placehold.it/32x32/ff8500',
-		large: 'http://placehold.it/128x128/ff8500'
+		large: 'http://placehold.it/64x64/ff8500'
 	}
 },{
 	id: 3,
 	name: "Tomatoes",
 	icon: {
 		small: 'http://placehold.it/32x32/ff0000',
-		large: 'http://placehold.it/128x128/ff0000'
+		large: 'http://placehold.it/64x64/ff0000'
 	}
 }];
